95f542544c970ff5d0aad02fa6c8ef48ba62e817
Yabuku-xD <shyamalankannan@gmail.com>
2024-10-29 15:42:07 -0700

    progress

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,15 +4,16 @@ function App() {
   return (
     <>
       <nav className="navbar">
-        <a>About</a>
-        <a>Experience</a>
+        <a href='#'>About</a>
+        <a href='#'>Experience</a>
         <p>Hey there!</p>
-        <a>Projects</a>
+        <a href='#'>Projects</a>
         <button>Contact</button>
         </nav>
       <div className="bgtext">
         <h1>Shyamalan</h1>
         <h1>Kannan</h1>
+        <img src="https://github.com/Yabuku-xD/Yabuku-xd.github.io/blob/main/portfolio/assets/me.gif" alt="ME" className ="gif"/>
       </div>
     </>
   );
