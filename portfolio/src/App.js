fb6261b53732b0c822118147db06704588da12d6
Yabuku-xD <shyamalankannan@gmail.com>
2024-10-29 14:51:49 -0700

    basic front

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <nav>
-      <a>About</a>
-      <a>Experience</a>
-      <a>Projects</a>
-      <a>Contact</a>
-    </nav>
+    <>
+      <nav className="navbar">
+        <a>About</a>
+        <a>Experience</a>
+        <p>Hey there!</p>
+        <a>Projects</a>
+        <button>Contact</button>
+        </nav>
+      <div className="bgtext">
+        <h1>Shyamalan</h1>
+        <h1>Kannan</h1>
+      </div>
+    </>
   );
 }
 
